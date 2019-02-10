a4902c3022e826629bf76adc31871af3efb46712
Michael Kemna <michael.kemna@gmail.com>
2019-02-10 15:37:37 +0100

    Initial attempt for tls support

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 ###########################################
+# EXPECTATIONS:
+# you have an helm chart for a flask app with uwsgi, and you are in that directory
+# you have a cluster-issuer-prod.yaml file, example found at:
+#	https://github.com/jldeen/croc-hunter/blob/master/cluster-issuer.yaml (please change email)
+
+###########################################
 # COMMAND LINE ARGUMENTS:
 # Expects the name of the app as the first argument
 NAME=$1
@@ -20,6 +26,9 @@ echo "Dry run settings set to $DRY_RUN"
 
 ############################################
 
+# Ensure that our repo is up to date
+helm repo update
+
 # Ensure that we have an nginx ingress controller on the cluster
 helm install stable/nginx-ingress --name my-nginx $DRY_RUN --set defaultBackend.enabled=false,controller.defaultBackendService="default/$1-flask-demo"
 echo "
@@ -28,7 +37,25 @@ Created the nginx-ingress
 ######################################
 "
 
-# Ensure that we have a cert-manager
+# Ensure that we have a cert-manager on the cluster
+helm install --name cert-mgr $DRY_RUN stable/cert-manager
+
+echo "
+######################################
+Created the cert-manager
+######################################
+"
+
+# Ensure that we have a certificate cluster issuer
+if ($2); then
+	kubectl apply -f cluster-issuer-prod.yaml
+	echo "
+	######################################
+	Created the certificate cluster issuer
+	######################################
+	"
+fi
+
 
 # Install the helm chart, expects to be in the current directory
 helm install --name $1 $DRY_RUN .
